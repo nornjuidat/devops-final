8171e42be03d53a37aa68beeeb301116c72a3a4b
nornjuidat <njuidatnor750@gmail.com>
2025-11-21 10:17:56 +0200

    tests

---
+++ src/math.js
@@ -0,0 +1,19 @@
+function sum(a,b)
+{
+    return a+b;   
+}
+
+function avg (a,b,c)
+{
+    return ((a+b+c)/3) ;
+}
+
+function mod (a,b,)
+{
+    return ((a+b)%10) ;
+}
+
+module.exports=
+{
+    sum,avg,mod
+}
