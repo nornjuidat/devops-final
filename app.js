72bd6465b8f72ad6abd9ac1e5105d4d61a64003f
nornjuidat <njuidatnor750@gmail.com>
2025-11-28 09:10:29 +0200

    node

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.send("Server is running!");
+});
+
+app.get("/api/test", (req, res) => {
+    res.json({ message: "API OK" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
