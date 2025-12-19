21493d1aab56942f1846bcce5e656f0d338706de
nornjuidat <njuidatnor750@gmail.com>
2025-12-19 09:11:38 +0200

    4

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 
+
+
+
 app.get("/", (req, res) => {
     res.send("Server is running!");
 });
