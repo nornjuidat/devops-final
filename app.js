f6c3ee4714fa177c1f286772318084cfe19a00db
nornjuidat <njuidatnor750@gmail.com>
2025-12-19 09:03:12 +0200

    3

--- app.js
+++ app.js
@@ -4,15 +4,20 @@ const path = require("path");
 const app = express();
 const PORT = 8080;
 
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+ 
 
 app.use(express.static(path.join(__dirname, "public")));
+
 
 app.get("/", (req, res) => {
     res.send("Server is running!");
 });
 
+   
 
 app.get("/api/test", (req, res) => {
     res.json({ message: "API OK" });
