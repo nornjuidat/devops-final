67f2b31cfad586194d251041719df3c2a625d790
nornjuidat <njuidatnor750@gmail.com>
2025-12-26 08:44:17 +0200

    new product

--- app.js
+++ app.js
@@ -4,28 +4,10 @@ const path = require("path");
 const app = express();
 const PORT = 8080;
 
-
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
- 
-
-app.use(express.static(path.join(__dirname, "public")));
-
-
-
-
-
 app.get("/", (req, res) => {
-    res.send("Server is running!");
-});
-
-   
-
-app.get("/api/test", (req, res) => {
-    res.json({ message: "API OK" });
+  res.sendFile(path.join(__dirname, "first.html"));
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
