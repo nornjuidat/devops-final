2cc38de1dcc79febb1215b6fb438e5d27fe8b267
nornjuidat <njuidatnor750@gmail.com>
2026-01-28 14:34:19 +0200

    Portfolio

--- app.js
+++ app.js
@@ -4,14 +4,13 @@ const path = require("path");
 const app = express();
 const PORT = 8080;
 
+// 🔑 מאפשר גישה לקבצים שנמצאים ליד first.html (img.jpg, css וכו')
+app.use(express.static(__dirname));
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "first.html"));
 });
 
-
-
-
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
-
